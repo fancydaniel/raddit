93f2126618d1b6b9e63f35ca366c95a3fb546c63
Daniel Uribe <duribe8@gmail.com>
2015-01-10 22:27:16 -0800

    Add names to user model and views**

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110234159) do
+ActiveRecord::Schema.define(version: 20150111055809) do
 
   create_table "comments", force: true do |t|
     t.integer  "link_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150110234159) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
